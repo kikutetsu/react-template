0552e8390596cede589b642a7ae8fe87964bb904
kikuchi <kikuchi.3989@icloud.com>
2018-03-24 11:33:18 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+  return (
+    <div className="test">
+
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />,
+     document.getElementById("app"));
